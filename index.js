a5b5f9f9ea231e784832e8b41c80670e6a450bc8
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-06-12 09:05:21 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(activity1 = 'roller-skate!') {
+    if (activity1 == 'roller-skate!') {
+        return `This Saturday, I want to ${activity1}`
+    } else {
+        return "This Saturday, I want to bathe my dog!";
+    }
+}
+
+const mondayWork = function(activity2 = 'go to the office') {
+    return `This Monday, I will ${activity2}.`
+}
+
+
+function wrapAdjective(symbol = '*') {
+    return function (adjective) {
+        return `You are ${symbol}${adjective}${symbol}!`;
+    }
+}
+
+let result = wrapAdjective('*')
+let emphatic = result("a hard worker")
+
